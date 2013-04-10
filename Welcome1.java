a6084e7b1c4748699635407332b32fa370230dc5
E Brisson test <ebrisson@bu.edu>
2013-04-10 20:15:57 -0300

    Update Welcome1.java

--- Welcome1.java
+++ Welcome1.java
@@ -7,7 +7,7 @@ public class Welcome1
    // main method begins execution of Java application
    public static void main( String args[] )
    {
-      System.out.println( "Welcome to Java Programming!" );
+      System.out.println( "Welcome to Java Programming!");
 
    } // end method main
 
